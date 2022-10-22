9fba09604a6b7ca16fd50ed930dd7a4d890aca9c
osorov97 <osorov.nursultan97@gmail.com>
2022-10-22 16:39:37 +0600

    finish

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,25 @@
 package peaksoft;
 
+import peaksoft.dao.UserDaoHibernateImpl;
+import peaksoft.service.UserService;
+import peaksoft.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        //   userDao.createUsersTable();
+        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+       userDaoHibernate.createUsersTable();
+
+        userDaoHibernate.saveUser("Nursultan", "Osorov", (byte)25);
+       userDaoHibernate.saveUser("Bekzad", "Sarybaev", (byte) 30);
+      userDaoHibernate.saveUser("Nurgazy", "Baetov", (byte) 29);
+      userDaoHibernate.saveUser("Mederbek", "Baitov", (byte) 35);
+
+    /* System.out.println(userService.getAllUsers());*/
+
+     //userService.cleanUsersTable();
+
+        //userService.dropUsersTable();
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,60 @@
 package peaksoft.util;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import peaksoft.model.User;
+
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    //    public static SessionFactory getSessionFactory() {
+//        Properties settings = new Properties();
+//        settings.put(Environment.DRIVER, "org.postgresql.Driver");
+//        settings.put(Environment.URL, "jdbc:postgresql://localhost:2022/postgres");
+//        settings.put(Environment.USER, "postgres");
+//        settings.put(Environment.PASS, "081262");
+//        settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+//        settings.put(Environment.SHOW_SQL, "true");
+//        settings.put(Environment.HBM2DDL_AUTO, "create");//validate //create
+//
+//
+//        Configuration configuration = new Configuration();
+//        configuration.addProperties(settings);
+//        configuration.addAnnotatedClass(User.class);
+//
+//        return configuration.buildSessionFactory();
+//    }
+//
+//    public static EntityManagerFactory entityManagerFactory() {
+//        return Persistence.createEntityManagerFactory("peaksoft");
+//    }
+    private static final SessionFactory sessionFactory = createsessionFactory();
+
+    public static SessionFactory createsessionFactory() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "org.postgresql.Driver");
+        properties.put(Environment.URL, "jdbc:postgresql://localhost:2022/postgres");
+        properties.put(Environment.USER, "postgres");
+        properties.put(Environment.PASS, "081262");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put(Environment.SHOW_SQL, "true");
+
+        Configuration configuration = new Configuration();
+        configuration.addProperties(properties);
+        configuration.addAnnotatedClass(User.class);
+
+        return configuration.buildSessionFactory();
+    }
+
+    public static EntityManagerFactory createEntityManagerFactory() {
+        return Persistence.createEntityManagerFactory("peaksoft");
+    }
 }
